413d7052e14f0630c606b44eabec044fae4698d7
Lewis Dale <lewis.dale@fortacode.co.uk>
2019-01-05 18:52:54 +0000

    Removed configuration package, replaced with cra app

--- packages/components/src/atoms/H1/H1.tsx
+++ packages/components/src/atoms/H1/H1.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { IH1Props } from './types';
 
+import './H1.scss';
+
 export const H1: React.SFC<IH1Props> = ({ children }) => (
-  <h1>{children}</h1>
+  <h1 className="h1">{children}</h1>
 );
--- packages/configuration/src/app.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import { H1 } from 'components';
-
-const div = document.createElement('div');
-document.body.appendChild(div);
-
-ReactDOM.render(<H1>Hello World</H1>, div);
